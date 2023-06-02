da2342889fd6b03290a9c932b32e77174dee32db
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-02 18:59:47 +0600

    [Done] Logger setup and customize logger message

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@ import mongoose from 'mongoose'
 import config from './config/index'
 
 import app from './app'
+import { errorLogger, logger } from './shared/Logger/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database Connected Successfully')
+    logger.info('Database Connected Successfully')
 
     // when Connected then listen app call
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Database Disconnected', err)
+    errorLogger.error('Database Disconnected', err)
   }
 }
 main()
