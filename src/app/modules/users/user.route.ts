6ad3c48e3f808ba13bcc5c5b59416192c0840573
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-06 02:56:20 +0600

    [Done] Zod Customize error Handler Added

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,8 +1,14 @@
-import express from 'express'
-import { UserController } from './user.controller'
+import express from 'express';
+import { UserController } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidation } from './user.validation';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create-user', UserController.createUser)
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+);
 
-export const UserRoutes = router
+export const UserRoutes = router;
