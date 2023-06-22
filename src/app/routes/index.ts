5682f8e69606a14eb3d9e12db7f62b029d1fcf15
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-22 18:40:49 +0600

    [Done] Updated Routes/index.ts file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFacult
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { StudentRoutes } from '../modules/student/student.route';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
 
 const router = express.Router();
 
@@ -32,6 +33,10 @@ const moduleRoutes = [
   {
     path: '/faculties',
     route: FacultyRoutes,
+  },
+  {
+    path: '/managementDepartments',
+    route: ManagementDepartmentRoutes,
   },
 ];
 moduleRoutes.forEach(route => router.use(route.path, route.route));
