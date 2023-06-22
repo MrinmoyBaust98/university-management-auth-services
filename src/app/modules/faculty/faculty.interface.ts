70f0430164d2507b0175b6e6454afe5ab13c23fa
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-22 15:11:11 +0600

    [Done] Faculty Interface & Model Added

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,40 @@
+import { Model, Types } from 'mongoose';
+import { IAcademicDepartment } from '../academicDepartment/academicDepartment.interfaces';
+import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interface';
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+};
+
+export type IFaculty = {
+  id: string;
+  name: UserName;
+  gender?: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  permanentAddress?: string;
+  presentAddress?: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  academicDepartment: Types.ObjectId | IAcademicDepartment;
+  academicFaculty: Types.ObjectId | IAcademicFaculty;
+  designation: string;
+};
+
+export type FacultyModel = Model<IFaculty, Record<string, unknown>>;
+
+export type IFacultyFilters = {
+  searchTerm?: string;
+  id?: string;
+  email?: string;
+  contactNo?: string;
+  emergencyContactNo?: string;
+  gender?: 'male' | 'female';
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  academicDepartment?: string;
+  academicFaculty?: string;
+  designation?: string;
+};
