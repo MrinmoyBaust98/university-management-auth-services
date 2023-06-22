9672dbf9febf4e01ff7d1df957541afcdee70cfd
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-23 04:02:54 +0600

    [Done] Admin section completed

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,18 +1,18 @@
 import express from 'express';
-// import validateRequest from '../../middlewares/validateRequest';
-// import { AdminController } from './admin.controller';
-// import { AdminValidation } from './admin.validation';
+import validateRequest from '../../middlewares/validateRequest';
+import { AdminController } from './admin.controller';
+import { AdminValidation } from './admin.validation';
 const router = express.Router();
 
-// router.get('/:id', AdminController.getSingleAdmin);
-// router.get('/', AdminController.getAllAdmins);
+router.get('/:id', AdminController.getSingleAdmin);
+router.get('/', AdminController.getAllAdmins);
 
-// router.delete('/:id', AdminController.deleteAdmin);
+router.delete('/:id', AdminController.deleteAdmin);
 
-// router.patch(
-//   '/:id',
-//   validateRequest(AdminValidation.updateAdmin),
-//   AdminController.updateAdmin
-// );
+router.patch(
+  '/:id',
+  validateRequest(AdminValidation.updateAdmin),
+  AdminController.updateAdmin
+);
 
 export const AdminRoutes = router;
