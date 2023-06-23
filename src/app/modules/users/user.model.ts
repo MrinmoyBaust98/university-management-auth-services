34f8cfe8b4db61c68d83bfd70292228739dd7aae
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-23 15:12:39 +0600

    [Done] password hashing

--- src/app/modules/users/user.model.ts
+++ src/app/modules/users/user.model.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Schema, model } from 'mongoose';
 import { IUser, UserModel } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
 
 const userSchema = new Schema<IUser>(
   {
@@ -17,6 +20,16 @@ const userSchema = new Schema<IUser>(
     },
   }
 );
+
+//hash password
+userSchema.pre('save', async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
 
 // Create  Model.
 export const User = model<IUser, UserModel>('User', userSchema);
