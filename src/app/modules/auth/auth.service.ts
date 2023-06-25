0a479dd9b19c1f987fdd6acc0b471b157bdad315
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-26 04:39:58 +0600

    [Done] AccessToken & Refresh Token Created. Modified User & Auth module

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import httpStatus from 'http-status';
 import ApiError from '../../../error/ApiError';
 import { User } from '../users/user.model';
-import { ILoginUser } from './auth.interface';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import { jwtHelpers } from '../../../helpers/jwthelpers';
 
-const loginUser = async (payload: ILoginUser) => {
+const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
   // jehetu id password checking common method ....sob jaygay use korbo..tai agulake inatance hisabe use korbo.....code see(user-interface,user-model)
 
@@ -20,7 +23,6 @@ const loginUser = async (payload: ILoginUser) => {
   }
 
   // Match Password
-
   if (
     isuserExist.password &&
     !user.isPasswordMatch(password, isuserExist?.password)
@@ -28,9 +30,32 @@ const loginUser = async (payload: ILoginUser) => {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'User password is not Vallid');
   }
 
-  // access Token Create
+  // data ---->isuserExist
+  // id previously used,so this process for 2nd use
+  const { id: userID, role, needsPasswordChange } = isuserExist;
 
-  return;
+  // as tokens are same type so we use reuseable function.
+  // we create it helpers folder...because it helps us to create token
+
+  // access Token Create
+  const accessToken = jwtHelpers.createToken(
+    { userID, role },
+    config.jwt.secret as Secret,
+    config.jwt.secret_expairs_in as string
+  );
+
+  // refresh access Token Create
+  const refreshAccessToken = jwtHelpers.createToken(
+    { userID, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_secret_expairs_in as string
+  );
+
+  return {
+    accessToken,
+    refreshAccessToken,
+    needsPasswordChange,
+  };
 };
 
 export const AuthService = {
